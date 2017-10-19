52065d47636abe04251282f6bfb73a8615e109bd
anzcarroll <anzcarroll@gmail.com>
2017-10-19 14:02:23 -0400

    was having issues with node server.js command, but npm run dev works just fine...not sure why it was reqd to use otherwise

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
+//import variables from .env file 
 require("dotenv").config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const app = express();
-mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI); //mongodb://localhost/idea-board
 
+mongoose.Promise = global.Promise;
+//create new app using express
+const app = express();
+
+// Connect to MongoDB and set up messages for when 
+// Mongo connects successfully or errors out
+mongoose.connect(process.env.MONGODB_URI); //mongodb://localhost/idea-board
 const connection = mongoose.connection;
+
 connection.on('connected', () => {
   console.log('Mongoose Connected Successfully');    
 }); 
@@ -20,6 +26,13 @@ app.use(bodyParser.json());
 app.get('/', (req,res) => {
   res.send('Hello world!')
 })
+
+// Inject middleware
+app.use(express.static(`${__dirname}/client/build`))
+app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/client/build/index.html`)
+})
+
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
